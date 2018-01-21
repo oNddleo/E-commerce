26f7e5849c52ed87d55b0a188be216b548292e08
oNddleo <nguyenduclong12a2nd@gmail.com>
2018-01-22 01:24:31 +0700

    user api update and delete

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -15,10 +15,30 @@ module.exports = {
         let data = req.body;
         User.create(data).exec((err, result) => {
             if(err){
-                
+
             }
+            //Should response message send client ,not send data
             return res.send(result);
         })
+    },
+    update: (req, res) => {
+        let data = req.body;
+        User.update(data).exec((err, result) => {
+            if(err){
+
+            }
+            res.send(result)
+        })
+    },
+    delete: (req, res) => {
+        let data = req.body;
+        User.destroy(data).exec((err, result) => {
+            if(err){
+
+            }
+            res.send(result)
+        })
     }
+    
 };
 
