ec945209b5839282536cc1138a9fda3940259cda
oNddleo <long.nguyenduc@fssc.com.vn>
2018-02-01 00:28:05 +0700

    create react redux

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+console.log('hey there!!');
+const App = () => {
+  return (
+    <div>
+      Simple Sails-React stater
+      Test Client App
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
