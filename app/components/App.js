f826f65bb8eeef2b80f70d6dd5f7697152a5ed5e
oNddleo <long.nguyenduc@stash.fss.com.vn>
2018-03-28 00:14:42 +0700

    add router in component reactjs

--- app/components/App.js
+++ app/components/App.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch, hasHistory, Layout } from 'react-router-dom';
-import Product from './Product/Product'
-import Menu from './Menu'
-import Upload from './Upload'
+import { BrowserRouter as Router, Route, Switch, hasHistory } from 'react-router-dom';
+import Product from './Product/Product';
+import Layout from './Layout';
 var store = require('store');
 export default class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                {/* <Router history={hasHistory}>
+                <Router history={hasHistory}>
                     <Layout>
                         <Switch>
-                            <Route exact path='/' />
+                            <Route exact path='/' component={Product}/>
                             <Route path='/roster' />
                             <Route path='/schedule' />
-                            <Route render={function () {
+                            {/* <Route render={function () {
                                 return (<Menu />)
-                            }} />
-                            
+                            }} /> */}
                         </Switch>
                     </Layout>
-                </Router> */}
-                
-                <Product />
-                
+                </Router>
             </Provider>
                 )
             }
