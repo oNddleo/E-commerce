833c65e90bd30f4eaefa897a09da6f30ba42e1c6
oNddleo <long.nguyenduc@stash.fss.com.vn>
2018-03-28 00:16:57 +0700

    add list banner image

---
+++ app/utils/banner-image.js
@@ -0,0 +1,18 @@
+export const listBannerImages = [
+    // {
+    //     original: '/images/banner/banner-advertisement.jpg',
+    //     thumbnail: '/images/banner/banner-advertisement.jpg',
+    // },
+    {
+        original: '/images/banner/banner-flash-sale.jpg',
+        thumbnail: '/images/banner/banner-flash-sale.jpg',
+    },
+    {
+        original: '/images/banner/banner-motor.jpg',
+        thumbnail: '/images/banner/banner-motor.jpg',
+    },
+    {
+        original: '/images/banner/banner-samsung-galaxy.jpg',
+        thumbnail: '/images/banner/banner-samsung-galaxy.jpg',
+    },
+]
