fc5de1f3a11a8047fcc4a9fc1502047975efd470
oNddleo <long.nguyenduc@stash.fss.com.vn>
2018-03-28 00:17:28 +0700

    layout website

---
+++ app/components/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import CardMostPopular from './components/CardMostPopular';
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div className="page">
+                <CardMostPopular />
+                <div>
+
+                </div>
+            </div>
+
+        )
+    }
+}
---
+++ app/components/Layout.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Menu from './Menu';
+import Banner from './Banner';
+import HomePage from './HomePage/HomePage';
+export default class Layout extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="header">
+          <Menu />
+        </div>
+        <div className="content">
+          <Banner />
+          <HomePage />
+        </div>
+        <div className="footer">
+
+        </div>
+      </div>
+    )
+  }
+};
