d94f21bafaf35d51b8de04c6dcba15ff38bbdc25
oNddleo <nguyenduclong12a2nd@gmail.com>
2018-04-12 16:57:51 +0700

    create officalstores

--- app/components/HomePage/components/OfficalStores.js
+++ app/components/HomePage/components/OfficalStores.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { Header } from 'app/components/Common/Header';
+import listCardOfficalStores from 'app/utils/listCardOfficalStores.json';
+import { CardOfficalStores } from 'app/components/Common/Card';
 export default class OfficalStores extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
+    render() {
+        return (
+            <div className="card-offical-stores">
+                <Header className="offical-stores" header='Gian hàng chính hãng' all={true} text='Xem thêm' link='/all'/>
+                <div className="card-offical-stores-content">
+                    <div className="card-offical-stores-content-body">
+                        {listCardOfficalStores.map(item =>
+                            <CardOfficalStores
+                                {...item}
+                                key={item.id}
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+};
