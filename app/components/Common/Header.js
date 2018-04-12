12bc34214c7fdba69ed8c924329eea310ca28ff8
oNddleo <nguyenduclong12a2nd@gmail.com>
2018-04-12 16:57:26 +0700

    change file header with card-shop-all

--- app/components/Common/Header.js
+++ app/components/Common/Header.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 
 export const Header = (props) => {
-    return (
-        <div className={"card-" + props.className + "-header"}>
-            <h3 className={"card-" + props.className + "-title"}>{props.header}</h3>
-        </div>
-    )
+    if(props.all){
+        return(
+            <div className={"card-" + props.className + "-header"}>
+                <h3 className={"card-" + props.className + "-title"}>{props.header}</h3>
+                <a href={props.link} className="card-shop-all">
+                    <span className="text">{props.text}</span>
+                    <i className="material-icons">&#xE315;</i>
+                </a>
+            </div>
+        )
+    }
+    else{
+        return (
+            <div className={"card-" + props.className + "-header"}>
+                <h3 className={"card-" + props.className + "-title"}>{props.header}</h3>
+            </div>
+        )
+    }
+    
 }
